4bce0ccead8f51e53ecae7ed31f28492af9f5573
JWMB <jonas.beckeman@gmail.com>
2022-12-04 11:08:12 +0100

    create_nextday command

---
+++ AofC_2022/Days/template.fs
@@ -0,0 +1,13 @@
+﻿module Template
+
+open Tools
+
+let part1 input =
+    let rows = Parsing.parseRows input (fun f -> f)
+    let result = 0
+    result
+    
+let part2 input =
+    let rows = Parsing.parseRows input (fun f -> f)
+    let result = 0
+    result
