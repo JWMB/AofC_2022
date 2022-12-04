f28a0c62b17b1e4e1cfb0c111d3bd9095491d2da
JWMB <jonas.beckeman@gmail.com>
2022-12-04 11:13:06 +0100

    template

--- AofC_2022/Days/template.fs
+++ AofC_2022/Days/template.fs
@@ -2,12 +2,14 @@
 
 open Tools
 
+let parseRow row = [| row |]
+
 let part1 input =
-    let rows = Parsing.parseRows input (fun f -> f)
+    let rows = Parsing.parseRows input parseRow
     let result = 0
     result
     
 let part2 input =
-    let rows = Parsing.parseRows input (fun f -> f)
+    let rows = Parsing.parseRows input parseRow
     let result = 0
     result
